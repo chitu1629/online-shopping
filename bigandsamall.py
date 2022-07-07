145c70d0b8af700412f3334b2bb2e54203605e9c
chitu1629 <chitralekha.rajkumar@indiumsoft.com>
2022-07-07 15:46:28 +0530

    python commit

---
+++ bigandsamall.py
@@ -0,0 +1,11 @@
+a = int (input('num 1'))
+b = int (input('num 2'))
+c = int (input('num 3'))
+
+if (a>b) and (a>c): print(a)
+if (b>a) and (b>c): print(b)
+if (c>a) and (c>b): print(c)
+
+if (a<b) and (a<c): print(a)
+if (b<a) and (b<c): print(b)
+if (c<a) and (c<b): print(c)
---
+++ state.py
@@ -0,0 +1,5 @@
+c = input('Enter the city names - Delhi or Agra or Jaipur:')
+if (c == 'Delhi'): print('Red Fort')
+elif(c == 'Agra'): print('Taj Mahal')
+elif(c == 'Jaipur'): print('Jal Mahal')
+else: print('Enter another city name!')
