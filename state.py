55393a5a757ddd84419358905e0fa41cf55e5e79
chitu1629 <108867392+chitu1629@users.noreply.github.com>
2022-07-07 17:21:15 +0530

    Update state.py

--- state.py
+++ state.py
@@ -1,3 +1,4 @@
+#Accept any city from the user and display monument of that city
 c = input('Enter the city names - Delhi or Agra or Jaipur:')
 if (c == 'Delhi'): print('Red Fort')
 elif(c == 'Agra'): print('Taj Mahal')
