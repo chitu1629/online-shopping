819cf858f360b43632c57d6346e0462dc4463fd8
chitu1629 <108867392+chitu1629@users.noreply.github.com>
2022-07-08 10:13:46 +0530

    Update bigandsamall.py

--- bigandsamall.py
+++ bigandsamall.py
@@ -1,12 +1,14 @@
-# this code prints the largest one
+# Getting input
 a = int (input('num 1'))
 b = int (input('num 2'))
 c = int (input('num 3'))
 
+#to find greater number
 if (a>b) and (a>c): print(a)
 if (b>a) and (b>c): print(b)
 if (c>a) and (c>b): print(c)
 
+  #to find smallest number
 if (a<b) and (a<c): print(a)
 if (b<a) and (b<c): print(b)
 if (c<a) and (c<b): print(c)
